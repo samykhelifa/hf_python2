7eaedb8a4c05423cc9a09728afe11d3a20772b44
samy <39716963+samykhelifa@users.noreply.github.com>
2019-12-20 06:18:34 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,8 +1,6 @@
 from datetime import datetime
-name = "sam"
-print(name)
-name = 23
-print(name)
+for num in range(5): 		
+    print('Head First Rocks!') 
 
 odds = [ 1,  3,  5,  7,  9, 11, 13, 15, 17, 19,        
         21, 23, 25, 27, 29, 31, 33, 35, 37, 39,       
