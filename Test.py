4d46dd7da000df70406e268bfee33d2dacd1ba74
samy <39716963+samykhelifa@users.noreply.github.com>
2019-12-24 01:22:11 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -4,11 +4,11 @@ import random,time
 odds = [ 1,  3,  5,  7,  9, 11, 13, 15, 17, 19,        
         21, 23, 25, 27, 29, 31, 33, 35, 37, 39,       
         41, 43, 45, 47, 49, 51, 53, 55, 57, 59 ]
-for num in range(5): 	
+for num in range(10): 	
    right_this_minute = datetime.today().minute
    if right_this_minute in odds:    
         print("This minute seems a little odd.") 
    else:   
         print("Not an odd minute.")
-   wait_time = random.randint(1,60)
+   wait_time = random.randint(1,30)
    time.sleep(wait_time)
