9764d027b9c33832627d1a0c75cbdfecb7ee638b
samy <39716963+samykhelifa@users.noreply.github.com>
2020-01-02 00:33:17 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,14 +1,9 @@
-from datetime import datetime
-import random,time
-	
-odds = [ 1,  3,  5,  7,  9, 11, 13, 15, 17, 19,        
-        21, 23, 25, 27, 29, 31, 33, 35, 37, 39,       
-        41, 43, 45, 47, 49, 51, 53, 55, 57, 59 ]
-for num in range(10): 	
-   right_this_minute = datetime.today().minute
-   if right_this_minute in odds:    
-        print("This minute seems a little odd.") 
-   else:   
-        print("Not an odd minute.")
-   wait_time = random.randint(1,30)
-   time.sleep(wait_time)
+vowels = ['a', 'e', 'i', 'o', 'u'] 
+word = "Milliways" 
+found = [] 
+for letter in word:    
+	if letter in vowels:         
+		if letter not in found:            
+			found.append(letter) 
+for vowel in found:     
+	print(vowel)
