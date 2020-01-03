95032c813885e2913fe02a66b64c0b98b5387880
samy <39716963+samykhelifa@users.noreply.github.com>
2020-01-03 09:45:46 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,10 +1,15 @@
-vowels = ['a', 'e', 'i', 'o', 'u'] 
-word = input("Provide a word to search for vowels: ")
-found = [] 
-for letter in word:    
-	if letter in vowels:         
-		if letter not in found:            
-			found.append(letter) 
-for vowel in found:     
-	print(vowel)
+phrase = "Don't panic!"
+plist = list(phrase)
+print(phrase)
+print(plist)
 
+for i in range(4):
+    plist.pop()
+plist.pop(0)
+plist.remove("'")
+plist.extend([plist.pop(), plist.pop()])
+plist.insert(2, plist.pop(3))
+
+new_phrase = ''.join(plist)
+print(plist)
+print(new_phrase)
