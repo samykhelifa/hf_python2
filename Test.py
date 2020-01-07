43254b1bf7b02b89053e573a4be3601a90826659
samy <39716963+samykhelifa@users.noreply.github.com>
2020-01-07 14:44:44 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,5 +1,10 @@
 vowels = ['a', 'e', 'i', 'o', 'u']
 word = "Milliways"
+found = [] 
 for letter in word:    
   if letter in vowels:         
-    print(letter)
+    if letter not in found:             
+      found.append(letter) 
+    
+for vowel in found:    
+      print(vowel)
