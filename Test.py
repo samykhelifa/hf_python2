8d02553a57cd7a427ce466f05b7c60fe5ebd707b
samy <39716963+samykhelifa@users.noreply.github.com>
2020-01-08 21:23:03 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,10 +1,17 @@
-paranoid_android = "Marvin, the Paranoid Android"
-letters = list(paranoid_android)
-for char in letters[:6]:    
-  print('\t', char)
-  print()
-for char in letters[-7:]:    
-  print('\t'*2, char) 
-  print() 
-for char in letters[12:20]:     
-  print('\t'*3, char)
+vowels = ['a', 'e', 'i', 'o', 'u'] 
+word = input("Provide a word to search for vowels: ")  
+
+found = {}  
+
+found['a'] = 0 
+found['e'] = 0 
+found['i'] = 0 
+found['o'] = 0 
+found['u'] = 0  
+
+for letter in word:     
+  if letter in vowels:         f
+    ound[letter] += 1  
+
+for k, v in  found :     
+  print(k, 'was found', v, 'time(s).')
