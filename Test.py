5b572d45a22455c838cf979f767150a128574584
samy <39716963+samykhelifa@users.noreply.github.com>
2020-01-10 04:49:50 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -3,11 +3,14 @@ word = input("Provide a word to search for vowels: ")
 
 found = {}  
 
-found['a'] = 0 
-found['e'] = 0 
-found['i'] = 0 
-found['o'] = 0 
-found['u'] = 0  
+#found['a'] = 0 
+#found['e'] = 0 
+#found['i'] = 0 
+#found['o'] = 0 
+#found['u'] = 0  
+
+for vowel in vowels:
+  found[vowel]=0
 
 for letter in word:     
   if letter in vowels:         
